116f23a254c64c28b717c6343c0bf4b1f85ca33f
MAS4soft <mas4soft@gmail.com>
2024-06-13 12:45:00 +0200

    last edit

---
+++ my-vite-react-app/src/landing/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+
+const Landing: React.FC = () => {
+  return (
+    <Grid container justifyContent="center" alignItems="center" style={{ height: '100vh' }}>
+      <Grid item>
+        hello world
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Landing;
+  
